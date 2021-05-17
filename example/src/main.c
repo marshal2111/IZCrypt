1737ba7eaa04c9d4cb7e1e58de0b4bb4164095ef
ilya <zaporozhecilya@gmail.com>
2021-05-18 01:51:13 +0300

    Example changed

--- example/src/main.c
+++ example/src/main.c
@@ -15,41 +15,53 @@ int main(int argc, char *argv[])
 		0xfc, 0xfd, 0xfe, 0xff
 
 	};
-	
-	const uint8_t ivSize = 19;
-	const uint8_t a[19] = {
+
+	const uint8_t inSize = 32;
+	const uint8_t a[32] = {
 		0xfe, 0xdc, 0xba, 0x98,
 		0x76, 0x54, 0x32, 0x10,
-		0xfe, 0xdc, 0xba, 0x98,
+		0x80, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x54, 0x32, 0x10, 0xfa,
 		0x76, 0x54, 0x32, 0x10,
-		0x54, 0x32, 0x10
+		0x80, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00
 	};
 
+	const uint8_t uIv[4] = {
+		0x12, 0x34, 0x56, 0x78
+	};
+	size_t sIvSize = 4;
+	uint8_t uS = 8;
+
 	printf("Vector value: ");
-	for (int i = 0; i < ivSize; i++)
+	for (int i = 0; i < inSize; i++)
 	{
 		printf("%X ", a[i]);
 	}
 	printf("\n");
 
-	uint8_t enc[ivSize];
-	uint8_t dec[ivSize];
+	uint8_t enc[inSize + 8];
+
 	size_t outSize = 0;
-	izStatus status;	
+	izStatus status;		
 
-	status = izEncrypt(izIdCipherAlgorithmMagma, izIdCipherModeECB, a, ivSize, key, 32, enc, &outSize);
+	status = izEncrypt(izIdCipherAlgorithmMagma, izIdCipherModeCTR, a, inSize, key, 32, uIv, sIvSize, uS, 0, enc, &outSize);
+
 	printf("status: %d\n", (uint8_t)status);
-	status = izDecrypt(izIdCipherAlgorithmMagma, izIdCipherModeECB, enc, outSize, key, 32, dec, outSize);	
-
-	printf("encrypted: "); 
+	printf("encrypted %d: ", outSize); 
 	for (int i = 0; i < outSize; i++)
 	{
 		printf("%X ", enc[i]);
 	}
 	printf("\n");
 
+	uint8_t dec[outSize];
+	status = izDecrypt(izIdCipherAlgorithmMagma, izIdCipherModeCTR, enc, outSize, key, 32, uIv, sIvSize, uS, 0, dec, &outSize);	
+
+	printf("status: %d\n", (uint8_t)status);
 	printf("decrypted: ");
-	for (int i = 0; i < ivSize; i++)
+	for (int i = 0; i < outSize; i++)
 	{
 		printf("%X ", dec[i]);
 	}
