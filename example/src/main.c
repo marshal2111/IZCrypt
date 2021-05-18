ce68265d79ebcdf09f80a1df1567d1f1ed837869
ilya <zaporozhecilya@gmail.com>
2021-05-18 03:44:18 +0300

    Example program updated

--- example/src/main.c
+++ example/src/main.c
@@ -1,6 +1,7 @@
 #include "IZCrypt.h"
 #include <stdio.h>
 #include <inttypes.h>
+#include <sys/stat.h>
 
 int main(int argc, char *argv[])
 {
@@ -16,54 +17,74 @@ int main(int argc, char *argv[])
 
 	};
 
-	const uint8_t inSize = 32;
-	const uint8_t a[32] = {
-		0xfe, 0xdc, 0xba, 0x98,
-		0x76, 0x54, 0x32, 0x10,
-		0x80, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00,
-		0x54, 0x32, 0x10, 0xfa,
-		0x76, 0x54, 0x32, 0x10,
-		0x80, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00
-	};
+	FILE* fp = fopen("../src/picture.jpg", "r");
 
+	if (fp == NULL) {
+        printf("File Not Found!\n");
+        return -1;
+    }
+
+    /***Считываем данные из файла***/
+	fseek(fp, 0L, SEEK_END);
+	long int FileSize = ftell(fp);
+	rewind(fp);
+
+	uint8_t* vIn = malloc(FileSize * sizeof(uint8_t));
+	fread(vIn, 1, FileSize, fp);
+	/*******************************/
+
+	/***Данные инициализации для CTR***/
 	const uint8_t uIv[4] = {
 		0x12, 0x34, 0x56, 0x78
 	};
 	size_t sIvSize = 4;
 	uint8_t uS = 8;
+	/*********************************/
 
-	printf("Vector value: ");
-	for (int i = 0; i < inSize; i++)
-	{
-		printf("%X ", a[i]);
-	}
-	printf("\n");
-
-	uint8_t enc[inSize + 8];
-
+	uint8_t* enc = malloc((FileSize + 1) * sizeof(uint8_t));	
 	size_t outSize = 0;
-	izStatus status;		
+	izStatus status;	
 
-	status = izEncrypt(izIdCipherAlgorithmMagma, izIdCipherModeCTR, a, inSize, key, 32, uIv, sIvSize, uS, 0, enc, &outSize);
-
-	printf("status: %d\n", (uint8_t)status);
-	printf("encrypted %d: ", outSize); 
-	for (int i = 0; i < outSize; i++)
-	{
-		printf("%X ", enc[i]);
+	/***Зашифрование данных***/
+	status = izEncrypt(izIdCipherAlgorithmMagma, izIdCipherModeCTR, vIn, FileSize, key, 32, uIv, sIvSize, uS, 0, enc, &outSize);
+	
+	printf("Status of encryption: %d\n", (uint8_t)status);
+	
+	if (status != IZStatusSuccess) {
+		printf("ENCRYPTION FAILED\n");
+		free(enc);
+		free(vIn);
+		return -1;
 	}
-	printf("\n");
 
-	uint8_t dec[outSize];
+	fp = fopen("../src/picture_encrypted", "w");
+	fwrite(enc, 1, outSize, fp);
+	fclose(fp);
+	/*************************/
+
+	uint8_t* dec = malloc(outSize * sizeof(uint8_t));
+
+	/***Расшифрование данных***/
 	status = izDecrypt(izIdCipherAlgorithmMagma, izIdCipherModeCTR, enc, outSize, key, 32, uIv, sIvSize, uS, 0, dec, &outSize);	
 
-	printf("status: %d\n", (uint8_t)status);
-	printf("decrypted: ");
-	for (int i = 0; i < outSize; i++)
-	{
-		printf("%X ", dec[i]);
+	if (status != IZStatusSuccess) {
+		printf("DECRYPTION FAILED\n");
+		free(enc);
+		free(vIn);
+		free(dec);
+		return -1;
 	}
-	printf("\n");
+
+	printf("Status of decryption: %d\n", (uint8_t)status);
+	fp = fopen("../src/picture_decrypted.jpg", "w");
+	fwrite(dec, 1, outSize, fp);
+	fclose(fp);
+	/*************************/
+
+
+	/*Высвобождение памяти*/
+	free(enc);
+	free(dec);
+	free(vIn);
+	/**********************/
 }
