605963db1ee1db1617ed9fc86198ee38fba8b914
ilya <zaporozhecilya@gmail.com>
2021-05-19 14:28:29 +0300

    Deleted excess info

--- include/IZCipher.h
+++ include/IZCipher.h
@@ -24,7 +24,7 @@ typedef enum izCipherMode_t {
 /** @brief Шифрование входного битового вектора "vIn" размера "sInSize", 
 *		результат запишется в выходной буффер "vOut" при достаточном размере указанном в "psOutSize".
 *		Алгоритм и режим шифрования указывается в параметрах "eAlgorithm", "eMode" соответственно.
-*		Для некоторых режимов шифрования необходимо передать вектор инициализации в "cvIv" размером "psIvSize".
+*		Для некоторых режимов шифрования необходимо передать вектор инициализации в "cvIv" размером "psIvSize", а так же дополнительные параметры..
 *		Если параметр "vOut" = null, то в параметре "psOutSize" будет указан необходимый размер буфера для шифрования вектора "cvIn".
 *		Фукнция запишет в "psOutSize" фактическое колличество зашифрованных байт.
 * 
@@ -59,7 +59,7 @@ izStatus izEncrypt(
 /** @brief Расфрование входного битового вектора "vIn" размера "sInSize",
 *		результат запишется в выходной буффер "vOut" при достаточном размере указанном в "psOutSize".
 *		Алгоритм и режим шифрования указывается в параметрах "eAlgorithm", "eMode" соответственно.
-*		Для некоторых режимов шифрования необходимо передать вектор инициализации в "cvIv" размером "psIvSize".
+*		Для некоторых режимов шифрования необходимо передать вектор инициализации в "cvIv" размером "psIvSize", а так же дополнительные параметры.
 *		Если параметр "vOut" = null, то в параметре "psOutSize" будет указан необходимый размер буфера для шифрования вектора "cvIn".
 *
 *	@param[in]		eAlgorithm	Алгоритм шифрования
@@ -70,6 +70,8 @@ izStatus izEncrypt(
 *	@param[in]		sKeySize	Размер ключевого вектора (в байтах)
 *	@param[in]		cvIv		Битовый вектор с вектором инициализации
 *	@param[in]		psIvSize	Размер вектора инициализации (в байтах)
+*	@param[in]		FirstParam 	Первый параметр режима шифрования (если необходим)
+*	@param[in]		FirstParam 	Второй параметр режима шифрования (если необходим)
 *	@param[out]		vOut		Выходной битовый вектор
 *	@param[in_out]	psOutSize	Размер выходного вектора (в байтах)
 *	@return Статус операции
@@ -87,96 +89,5 @@ izStatus izDecrypt(
 	uint16_t SecondParam,
 	void* vOut,
 	size_t* psOutSize);
-
-// * @brief Структура для хранения контекста шифрования 
-// typedef struct izEncryptionCtx_t izEncryptionCtx;
-
-/** @brief Инициализация контекста для поточного шифрования
-*
-*	@param[in_out]	sCtx	Контекст шифрования
-*	@return Статус операции
-*/
-// izStatus izEncryptionCtxInit(
-// 	_In_out izEncryptionCtx* sCtx);
-
-// /** @brief Очистка контекста шифрования
-// *
-// *	@param[in_out]	sCtx	Контекст шифрования
-// *	@return Статус операции
-// */
-// izStatus izEncryptionCtxFree(
-// 	_In_ izEncryptionCtx* sCtx);
-
-// /** @brief Ключи параметров (Принимающих конктерное значение)*/
-// typedef enum izCipherPropertyName_t {
-// 	izCipherPropertyAlgorithm,
-// 	izCipherPropertyMode
-// } izCipherPropertyName;
-
-// /** @brief Установка параметров в контекст
-// *
-// *	@param[in_out]	sCtx			Контекст
-// *	@param[in]		ePropertyKey	Ключ параметра
-// *	@param[in]		strValue		Идентификатор значения параметра
-// *	@return Статус операции
-// */
-// izStatus izEncryptionSetProperty(
-// 	_In_out	izEncryptionCtx			sCtx, 
-// 	_In_	izCipherPropertyName	ePropertyKey,
-// 	_In_	const char*				strValue);
-
-// /** @brief Ключи параметров (Битовые вектора) */
-// typedef enum izCipherPropertyByteVectorName_t {
-// 	izCipherPropertyIV,
-// 	izCipherPropertyKey
-// } izCipherPropertyByteVectorName;
-
-// /** @brief Установка параметров в контекст
-// *
-// *	@param[in_out]	sCtx			Контекст
-// *	@param[in]		ePropertyKey	Ключ параметра
-// *	@param[in]		cvValue			Битовый вектор - значение параметра
-// *	@return Статус операции
-// */
-// izStatus izEncryptionSetPropertyByteVector(
-// 	_In_out	izEncryptionCtx					sCtx,
-// 	_In_	izCipherPropertyByteVectorName	ePropertyKey,
-// 	_In_	const void*						cvValue);
-
-// * @brief Поточное шифрование входного блока "cvIn", дополнение последнего блока осуществляться не будет.
-// *		Блок будет сохранён и добавлен к следующем вектору полученному из следующего izEncryptionUpdate().
-// *		Если параметр "vOut"=null, в параметр "psOutSize" будет записан размер массива необходимый для получения выходного значения.
-// *		Функция записывает в "psOutSize" фактическое колличество зашифрованных байт.
-// * 
-// *	@param[in]		sCtx		Контекст шифрования
-// *	@param[in]		cvIn		Входной битовый вектор
-// *	@param[in]		sInSize		Размер входного вектора (в байтах)
-// *	@param[out]		vOut		Выходной битовый вектор
-// *	@param[in_out]	psOutSize	Размер выходного вектора (в байтах)
-// *	@return Статус операции
-
-// izStatus izEncryptionUpdate(
-// 	_In_	izEncryptionCtx	sCtx,
-// 	_In_	const void*		cvIn,
-// 	_In_	size_t			sInSize,
-// 	__out	void*			vOut,
-// 	_In_out	size_t*			psOutSize);
-
-// /** @brief Завершаюший Update поточного шифрования. Последний блок будет по необходимости дополнен до полного блока и зашифрован.
-// *		Если параметр "vOut"=null, в параметр "psOutSize" будет записан размер массива необходимый для получения выходного значения.
-// *		Функция записывает в "psOutSize" фактическое колличество зашифрованных байт.
-// *
-// *	@param[in]		sCtx		Контекст шифрования
-// *	@param[in]		cvIn		Входной битовый вектор
-// *	@param[in]		sInSize		Размер входного вектора (в байтах)
-// *	@param[out]		vOut		Выходной битовый вектор
-// *	@param[in_out]	psOutSize	Размер выходного вектора (в байтах)
-// *	@return Статус операции
-// */
-// izStatus izEncryptionFinalUpdate(_In_	izEncryptionCtx sCtx,
-// 	_In_	const void*	cvIn,
-// 	_In_	size_t		sInSize,
-// 	__out	void*		vOut,
-// 	_In_out	size_t*		psOutSize);
 
 #endif //!IZCIPHER_H
