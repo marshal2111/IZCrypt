be7bfbfbe6338b8d623356d96a2d6ce36cfac38e
ilya <zaporozhecilya@gmail.com>
2021-05-19 14:31:27 +0300

    Minor fix

--- src/h/cipher/IZMagma.h
+++ src/h/cipher/IZMagma.h
@@ -8,16 +8,14 @@
 *	@param[in]  in		64-х битный блок на вход
 *	@param[in]	key		256-ти битный ключ для операции
 *	@param[out]	out		64-x битный блок на выход
-*	@return Статус операции
 */
-void izMagmaEncrypt(uint8_t* key, const uint8_t* in, uint8_t* out); //TODO return status
+void izMagmaEncrypt(uint8_t* key, const uint8_t* in, uint8_t* out);
 
 /** @brief Расшифрование одного блока алгоримом Магма 34.12-2015 n = 64
 *
 *	@param[in]  in		64-х битный блок на вход
 *	@param[in]	key		256-ти битный ключ для операции
 *	@param[out]	out		64-x битный блок на выход
-*	@return Статус операции
 */
 void izMagmaDecrypt(uint8_t* key, const uint8_t* in, uint8_t* out);
 
